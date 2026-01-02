8d3e4d024aecc909af50a8344c910abd0caa0712
Semih Şahinoğlu <semihshngl@gmail.com>
2026-01-02 16:49:51 +0300

    dosyalar eklendi

---
+++ src/main/java/com/semihsahinoglu/teams_service/TeamsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.semihsahinoglu.teams_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semihsahinoglu/teams_service/TeamsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semihsahinoglu.teams_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
